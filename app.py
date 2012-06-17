b50f5a32287686d5284bc33b2810ce298fc71d97
Luke Lee <durdenmisc@gmail.com>
2012-06-17 17:24:46 -0500

    Add basic unstyled flask app for web interface

---
+++ app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""
+Tiny web interface around notmyidea.py
+"""
+
+from flask import Flask, render_template, request
+
+from notmyidea import get_user_forks, get_user_contributions
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def home():
+    """homepage"""
+
+    return render_template('index.html')
+
+
+@app.route('/lookup')
+def lookup():
+    """Lookup github user's contributions"""
+
+    # FIXME: Handle errors
+
+    if request.method == 'GET':
+        username = request.args.get('github_user', None)
+        if username is None:
+            return None
+
+        fork_urls = get_user_forks(username)
+
+        contributions = []
+        for url, cnt, commits_url in get_user_contributions(username, fork_urls):
+            contributions.append({'url': url, 'commit_cnt': cnt,
+                                  'commits_url': commits_url})
+
+        return render_template('contributions.html', user=username,
+                               contributions=contributions)
+
+
+def run_web(host, port):
+    """Run web interface"""
+
+    app.run(host=host, port=port)
+
+
+if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get('PORT', 5000))
+    run_web(host='0.0.0.0', port=port)
